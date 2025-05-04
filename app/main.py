b72abc728326ff0021c8485eeda6a6fd09585c46
Deepakpal0807 <Deepakpal981790@gmail.com>
2025-05-04 22:06:34 +0530

    ploty

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pickle
 import pandas as pd
-import plotly.graph_objects as go
+# import plotly.graph_objects as go
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
